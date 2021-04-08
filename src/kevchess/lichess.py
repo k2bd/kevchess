f0db882ed4d30aef5508a9f01c14fc6ea21b9216
Kevin Duff <kevinkelduff@gmail.com>
2021-04-08 20:08:53 +0100

    Removed async attempt 2

--- src/kevchess/lichess.py
+++ src/kevchess/lichess.py
@@ -7,6 +7,7 @@ from mcts import Mcts
 from node import Node
 from chess import Board, Move, STARTING_FEN
 import aiohttp
+import time
 
 token = os.environ["LICHESS_BOT_TOKEN"]
 
@@ -38,74 +39,18 @@ def get_move(old_node: Node, new_node: Node) -> str:
         )
 
 
-class AsnycGame:
-    def __init__(self, game_id, player_id, explore_weight=1.0):
+class Game:
+    def __init__(self, client: berserk.Client, game_id, player_id, explore_weight=1.0):
+        self.client = client
+
         self.game_id = game_id
         self.player_id = player_id
         self.explore_weight = explore_weight
 
-        self.playing = False
+        self.stream = client.bots.stream_game_state(game_id)
 
-    def start(self, loop=None):
-        loop = loop or asyncio.get_event_loop()
-
-        loop.run_until_complete(self.connect())
-
-        self.learn_task = loop.create_task(self.learn())
-        self.events_task = loop.create_task(self.handle_events())
-        self.make_moves_task = loop.create_task(self.make_moves())
-
-        print("Game started")
-
-        loop.run_forever()  # For now, block and run through the whole game
-
-    def stop(self):
-        self.learn_task.cancel()
-        self.events_task.cancel()
-        self.make_moves_task.cancel()
-
-    async def connect(self):
-        self.session = aiohttp.ClientSession(headers={"Authorization": f"Bearer {token}"})
-
-    async def learn(self):
-        while True:
-            if self.playing:
-                print("Learning!")
-                self.tree.rollout(self.node)
-            await asyncio.sleep(0)
-
-    async def handle_events(self):
-        async with self.session.get(f'https://lichess.org/api/bot/game/stream/{self.game_id}') as r:
-            async for raw_event in r.content:
-                event = json.loads(raw_event)
-                print("Got event: ", event)
-                if event['type'] == "gameFull":
-                    self.handle_game_info(event)
-                elif event['type'] == 'gameState':
-                    self.handle_state_change(event)
-                elif event['type'] == 'chatLine':
-                    self.handle_chat_line(event)
-                await asyncio.sleep(0)
-
-    async def make_moves(self) -> None:
-        while True:
-            if self.playing and self.my_turn:
-                print("trying to make move")
-                new_node = self.tree.choose(self.node)
-
-                # Make the selected move
-                move_str = get_move(self.node, new_node)
-                print("Making move", move_str)
-                await self.session.post(
-                    f"https://lichess.org/api/bot/game/{self.game_id}/move/{move_str}"
-                )
-
-                self.node = new_node
-                self.my_turn = not self.my_turn
-            await asyncio.sleep(0)
-
-    def handle_game_info(self, event):
-        self.initial_fen = event["initialFen"]
+        game_info = next(self.stream)
+        self.initial_fen = game_info["initialFen"]
         if self.initial_fen == "startpos":
             self.initial_fen = STARTING_FEN
 
@@ -113,45 +58,53 @@ class AsnycGame:
 
         self.tree = Mcts(explore_weight=self.explore_weight)
         self.node = Node(fen=self.initial_fen)
+        self.turn_speed_seconds = 10  # TODO
 
-        self.my_turn = event["white"]["id"] == self.player_id
+        self.my_turn = game_info["white"]["id"] == self.player_id
+        if self.my_turn:
+            self.make_move()
 
-        self.turn_speed = 10
         #self.turn_speed = self.initial_state["clock"]["increment"]
 
-        self.playing = True
+    def run(self):
+        for event in self.stream:
+            print("Got event: ", event)
+            if event['type'] == 'gameState':
+                self.handle_state_change(event)
+            elif event['type'] == 'chatLine':
+                self.handle_chat_line(event)
 
-    def handle_state_change(self, game_state):
+    def make_move(self):
+        start = time.time()
+        print("Thinking...")
+        while time.time() - start < self.turn_speed_seconds:
+            self.tree.rollout(self.node)
+
+        new_node = self.tree.choose(self.node)
+
+        # Make the selected move
+        move_str = get_move(self.node, new_node)
+        print("Making move", move_str)
+        self.client.bots.make_move(self.game_id, move_str)
+
+        self.node = new_node
+
+    def handle_state_change(self, event):
         board = Board(fen=self.initial_fen)
-        for move in game_state["moves"].split():
+        for move in event["moves"].split():
             board.push(Move.from_uci(move))
         self.node = Node(fen=board.fen())
+        self.my_turn = not self.my_turn
 
-        self.my_turn = True
+        print(f"My turn? {self.my_turn}")
 
-    def handle_chat_line(self, chat_line):
-        print(chat_line)
+        if self.my_turn:
+            self.make_move()
 
 
-class Game:
-    def __init__(self, game_id, player_id, explore_weight=1.0):
-        self.session = requests.Session()
-        self.session.headers.update({"Authorization": f"Bearer {token}"})
 
-        self.game_id = game_id
-        self.player_id = player_id
-        self.explore_weight = explore_weight
-
-        self.stream = self._stream_events()
-
-        game_info = next(self.stream)
-        
-
-    def _stream_events(self):
-        yield from self.session.get(
-            f'https://lichess.org/api/bot/game/stream/{self.game_id}',
-            stream=True,
-        )
+    def handle_chat_line(self, event):
+        print(event)
 
 
 def accept_challenge(event) -> bool:
@@ -174,5 +127,5 @@ if __name__ == "__main__":
             elif is_polite:
                 client.bots.decline_challenge(event["challenge"]['id'])
         elif event['type'] == 'gameStart':
-            game = AsnycGame(event["game"]['id'], player_id=my_id)
-            game.start()
+            game = Game(client, event["game"]['id'], player_id=my_id)
+            game.run()
